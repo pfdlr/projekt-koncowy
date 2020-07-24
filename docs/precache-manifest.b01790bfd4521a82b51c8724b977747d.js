8e7fe29c171ca52550f49fe9a17cb309ffcdaa91
Piotr Fidler <fidler.piotr@gmail.com>
2020-07-24 14:14:18 +0200

    add build in docs/

---
+++ docs/precache-manifest.b01790bfd4521a82b51c8724b977747d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb8cccf299252eaecf778ec15d84443",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37be46e9f652a9ea2c1f",
+    "url": "/static/css/2.e550d8d4.chunk.css"
+  },
+  {
+    "revision": "d9c0bf99626479e007fc",
+    "url": "/static/css/main.dcd2a7e4.chunk.css"
+  },
+  {
+    "revision": "37be46e9f652a9ea2c1f",
+    "url": "/static/js/2.15cb29a7.chunk.js"
+  },
+  {
+    "revision": "d9c0bf99626479e007fc",
+    "url": "/static/js/main.77f95f73.chunk.js"
+  },
+  {
+    "revision": "e6648bac1caba1057cf7",
+    "url": "/static/js/runtime-main.fbb813f8.js"
+  }
+]);
